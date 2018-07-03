12a77524f5c4b2efe0abfba34065aa6579e5f2d2
cupofjoy <eelantung@gmail.com>
2018-07-03 13:46:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 // Code Goes Here
+import React, { Component } from 'react';
+
+export default class MovieBrowser extends Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isPlaying: child.props.title === currentPlayingTitle
+      });
+    });
+
+    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+      return (
+        <div className="some-component-special-class">{child}</div>
+      )
+    })
+
+    return (
+      <div className="some-component">
+        <p>This component has {React.Children.Count(this.props.children)} children.</p>
+        {childrenWithWrapperDiv}
+      </div>
+    );
+  }
+}
